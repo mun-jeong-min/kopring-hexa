e84e90100e88618c808d283aa1dbe98d1e608f7d
문정민 <moondeve41@gmail.com>
2022-12-21 17:25:06 +0900

    ADD :: ErrorCode 정의

---
+++ hexa-infrastructure/src/main/kotlin/com/example/kopringhexa/global/error/custom/ErrorCode.kt
@@ -0,0 +1,18 @@
+package com.example.kopringhexa.global.error.custom
+
+import org.springframework.http.HttpStatus
+
+enum class ErrorCode (
+        private val status: HttpStatus,
+        private val message: String
+) : ErrorProperty {
+    USER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "user not found"),
+
+    INVALID_JWT_EXCEPTION(HttpStatus.UNAUTHORIZED, "invalid jwt"),
+    EXPIRED_JWT_EXCEPTION(HttpStatus.UNAUTHORIZED, "Expired jwt"),
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "server error");
+
+    override fun status() = status.value()
+    override fun message() = message
+}
---
+++ hexa-infrastructure/src/main/kotlin/com/example/kopringhexa/global/error/custom/ErrorProperty.kt
@@ -0,0 +1,6 @@
+package com.example.kopringhexa.global.error.custom
+
+interface ErrorProperty {
+    fun status(): Int
+    fun message(): String
+}
