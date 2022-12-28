504be652fe67d32f9ee6ab0dd63059a93eae38d3
문정민 <moondeve41@gmail.com>
2022-12-28 16:12:53 +0900

    EDIT :: index 속성 추가

--- hexa-infrastructure/src/main/kotlin/com/example/kopringhexa/domain/search/persistence/entity/SearchEntity.kt
+++ hexa-infrastructure/src/main/kotlin/com/example/kopringhexa/domain/search/persistence/entity/SearchEntity.kt
@@ -3,11 +3,13 @@ package com.example.kopringhexa.domain.search.persistence.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
 import javax.validation.constraints.NotBlank
 
 @RedisHash(value = "search")
 class SearchEntity (
         @Id
+        @Indexed
         val userId: Long,
 
         @NotBlank
